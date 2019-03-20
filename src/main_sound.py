2b070894dcf83393704cb5c12c8eb21f748053aa
Rémi Taunay <remi.taunay@gmail.com>
2019-03-20 17:01:55 +0100

    several sounds @ the same time

--- src/main_sound.py
+++ src/main_sound.py
@@ -3,22 +3,31 @@
 import time
 from openal import *
 
-dname = alcGetString(None, ALC_DEVICE_SPECIFIER)
-print(f'device name: {dname.decode("utf-8")}')
-device = alcOpenDevice(dname)
+CENTER = 0
+LEFT = FRONT = -2
+RIGHT = BACK = 2
 
-ctx_listener = oalGetListener()
-ctx_listener.set_position((0, 0, 0))
-ctx_listener.set_velocity((0, 0, 0))
-ctx_listener.set_orientation((0, 0, 1, 0, 1, 0))
+def play(filepath, loop=False, left_center_right=CENTER, back_center_front=CENTER):
+	s = oalOpen(filepath)
+	s.set_looping(loop)
+	s.set_position((left_center_right, CENTER, back_center_front))
+	s.play()
 
-s0 = oalOpen('res/gas.flac')
-s0.set_looping(True)
-s0.set_position((0,0,0))
-s0.play()
+if __name__ == '__main__':
 
-while True:
-	time.sleep(0.02)
-	print(s0.position)
+	dname = alcGetString(None, ALC_DEVICE_SPECIFIER)
+	print(f'device name: {dname.decode("utf-8")}')
+	device = alcOpenDevice(dname)
 
-alcCloseDevice(device)
+	ctx_listener = oalGetListener()
+	ctx_listener.set_position((CENTER, CENTER, CENTER))
+	ctx_listener.set_velocity((0, 0, 0))
+	ctx_listener.set_orientation((0, 0, 1, 0, 1, 0))
+
+	play('res/gas.flac', left_center_right=LEFT, back_center_front=BACK)
+	play('res/discord.flac', left_center_right=RIGHT, back_center_front=FRONT)
+
+	while True:
+		time.sleep(1)
+
+	alcCloseDevice(device)
