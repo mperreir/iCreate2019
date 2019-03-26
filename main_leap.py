8e017c77236e2eb41af1082f6cb48ba6f04dcb1b
SDS <SDS>
2019-03-26 21:44:50 +0100

    Squelette de script complet créé

--- main_leap.py
+++ main_leap.py
@@ -14,15 +14,70 @@ async def main_procedure():
         print(data)
         await websocket.send(sub)
 
-        print('coucou')
+        print('Track 1')
+        # TODO Insert track 1 -> "Hé, toi là..."
         actionPerformed = False
+        while not actionPerformed:
+            data = await websocket.recv()
+            data = json.loads(data)
+            actionPerformed = read_action_from_leap(data, 'grabStrength')
+        await websocket.send(unSub)
+        print('Track 2')
+        #TODO Insert track 2 -> "Super ! Merci beaucoup"
+        await websocket.send(sub)
         while not actionPerformed:
             data = await websocket.recv()
             data = json.loads(data)
             actionPerformed = read_action_from_leap(data, 'pinchStrength')
         await websocket.send(unSub)
-        print('coucou2')
+        print('Track 3')
+        print('Track 4 (Son zwoosh)')
+        print('Track 5')
+        print('Track 6 -> Viens voir ici')
+        print('Track 7 -> COmmence par glisser ta main')
         await websocket.send(sub)
+        while not actionPerformed:
+            data = await websocket.recv()
+            data = json.loads(data)
+            actionPerformed = read_action_from_leap(data, 'moveLeft')
+        await websocket.send(unSub)
+        print('Sons orientation telescope')
+        print('Track Super')
+        print('Bruit de vent')
+        print('Track Brrrr il fait froid')
+        await websocket.send(sub)
+        while not actionPerformed:
+            data = await websocket.recv()
+            data = json.loads(data)
+            actionPerformed = read_action_from_leap(data, 'handDetected')
+        await websocket.send(unSub)
+        print('Son téléscope qui s\'ouvre')
+        print('Track balaie la main devant toi')
+        await websocket.send(sub)
+        while not actionPerformed:
+            data = await websocket.recv()
+            data = json.loads(data)
+            actionPerformed = read_action_from_leap(data, 'handLeft')
+        await websocket.send(unSub)
+        print('Track Trappist1E')
+        print('Track balaie la main devant toi')
+        await websocket.send(sub)
+        while not actionPerformed:
+            data = await websocket.recv()
+            data = json.loads(data)
+            actionPerformed = read_action_from_leap(data, 'handLeft')
+        await websocket.send(unSub)
+        print('Track Kepler-186f')
+        print('Track balaie la main devant toi dernière planèye')
+        await websocket.send(sub)
+        while not actionPerformed:
+            data = await websocket.recv()
+            data = json.loads(data)
+            actionPerformed = read_action_from_leap(data, 'handLeft')
+        await websocket.send(unSub)
+        print('Track Kepler 16b')
+        print('Track fin du jeu')
+
 
 def format_json(data):
     file = open('JSONParsed', 'w')
@@ -33,6 +88,8 @@ def format_json(data):
 def read_action_from_leap(data, action_string):
         if action_string == 'grabStrength':
             return data['hands'] and (data['hands'][0][action_string] == 1.0)
+        elif action_string == 'handDetected':
+            return data['hands']
         elif action_string == 'pinchStrength':
             return data['hands'] and (data['hands'][0]['grabStrength']) < 0.5 and (data['hands'][0][action_string] == 1.0)
         elif action_string == 'moveRight':
