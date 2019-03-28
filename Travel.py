eb8b8354bc64ce4893a5a841ea63271f25152ee8
Yves Le Palud <yveslepalud@MacBook-Pro-de-Yves.local>
2019-03-28 16:10:09 +0100

    Refactor

--- Travel.py
+++ Travel.py
@@ -23,36 +23,19 @@ class Travel:
         context_listener.velocity = 0, 0, 0
         context_listener.orientation = 0, 0, 0, 0, 0, 0
 
-        source_path = "avion" + str(randint(1, 2)) + ".flac"
-        print(source_path)
-        source = oalOpen(source_path)
-        source.set_position(self.start_position)
-        source.set_cone_inner_angle(360)
-        source.set_cone_outer_angle(360)
-
-        if self.end_position[0] > self.start_position[0]:
-            factor_x = 1
-        else:
-            factor_x = -1
-
-        if self.end_position[2] > self.start_position[2]:
-            factor_y = 1
-        else:
-            factor_y = -1
-
-        self.move(source,
-                  (abs(self.end_position[0]) + abs(self.start_position[0])) / 40 * factor_x,
-                  (abs(self.end_position[2]) + abs(self.start_position[2])) / 40 * factor_y)
+        self.sound_fly()
         self.sound_information()
 
-    def move(self, source, step_x, step_y):
+    def sound_fly(self) -> None:
         """
         Play the sound to the expected destination
-        :param source: The source of the sound
-        :param step_x: The sound movement in the x direction
-        :param step_y: The sound movement in the y direction
-        :return:
         """
+        source_path = "avion" + str(randint(1, 2)) + ".flac"
+        print(source_path)
+        source = self._create_sound_source(source_path, self.start_position)
+
+        step_x, step_y = self._get_factor_step(40)
+
         source.play()
         counter_x = source.position[0]
         counter_y = source.position[2]
@@ -69,12 +52,32 @@ class Travel:
         """
         PLay the information corresponding to the city
         """
-        source = oalOpen(self.sound_path)
-        source.set_position(self.end_position)
-        source.set_cone_inner_angle(360)
-        source.set_cone_outer_angle(360)
+        source = self._create_sound_source(self.sound_path, self.end_position)
+        source.play()
 
         while source.get_state() == AL_PLAYING:
-            time.sleep(0.1)
+            time.sleep(0.2)
 
         source.stop()
+
+    def _create_sound_source(self, source, position) -> Source:
+        source = oalOpen(source)
+        source.set_position(position)
+        source.set_cone_inner_angle(360)
+        source.set_cone_outer_angle(360)
+        return source
+
+    def _get_factor_step(self, step) -> (float, float):
+        if self.end_position[0] > self.start_position[0]:
+            factor_x = 1
+        else:
+            factor_x = -1
+
+        if self.end_position[2] > self.start_position[2]:
+            factor_y = 1
+        else:
+            factor_y = -1
+
+        step_x = (abs(self.end_position[0]) + abs(self.start_position[0])) / step * factor_x
+        step_y = (abs(self.end_position[2]) + abs(self.start_position[2])) / step * factor_y
+        return step_x, step_y
