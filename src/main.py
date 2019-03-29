b5eb057c20701d98915902ac6ae61f6565926d5d
SDS <SDS>
2019-03-29 11:42:43 +0100

    tentative de correction windows

--- src/main.py
+++ src/main.py
@@ -12,10 +12,31 @@ BLA_DIR = join(BLA_DIR, lang)
 
 
 def format_sound_call(counter):
-    return 'phrase'+counter+'.flac'
+    return 'phrase' + counter + '.flac'
 
 
 async def main_procedure():
+    trackno = 1
+    async with websockets.connect('ws://127.0.0.1:6437/v6.json') as websocket:
+        sub = json.dumps({'focused': True})
+        unsub = json.dumps({'focused': False})
+        data = await websocket.recv()
+        print(data)
+        data = await websocket.recv()
+        print(data)
+        await websocket.send(sub)
+
+        ambiant = play(join(MUS_DIR, 'coaxial.flac'), loop=True)
+        ambiant.set_gain(0.05)
+        play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
+        trackno += 1
+        actionPerformed = False
+        while not actionPerformed:
+            data = await websocket.recv()
+            data = json.loads(data)
+            actionPerformed = read_action_from_leap(data, 'grabStrength')
+        play(join(SFX_DIR, 'confirm.flac'), until_end=True)
+        ambiant.set_gain(0.1)
 
     async with websockets.connect('ws://127.0.0.1:6437/v6.json') as websocket:
         sub = json.dumps({'focused': True})
@@ -25,25 +46,8 @@ async def main_procedure():
         data = await websocket.recv()
         print(data)
         await websocket.send(sub)
-        trackno = 1
 
-        ambiant = play(join(MUS_DIR, 'coaxial.flac'), loop=True)
-        ambiant.set_gain(0.05)
-        play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
-        trackno += 1
-        change_gif('poing')
-        actionPerformed = False
-        while not actionPerformed:
-            data = await websocket.recv()
-            data = json.loads(data)
-            actionPerformed = read_action_from_leap(data, 'grabStrength')
-        await websocket.send(unsub)
-        while await websocket.recv():
-            pass
-        play(join(SFX_DIR, 'confirm.flac'), until_end=True)
-        ambiant.set_gain(0.1)
-
-        #Recherche signe et reproduction
+        # Recherche signe et reproduction
         play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
         trackno += 1
         print('Began looking')
@@ -60,14 +64,22 @@ async def main_procedure():
             data = json.loads(data)
             actionPerformed = read_action_from_leap(data, 'pinchStrength')
         await websocket.send(unsub)
-        while await websocket.recv():
-            pass
         play(join(SFX_DIR, 'confirm.flac'), until_end=True)
+
+    async with websockets.connect('ws://127.0.0.1:6437/v6.json') as websocket:
+        sub = json.dumps({'focused': True})
+        unsub = json.dumps({'focused': False})
+        data = await websocket.recv()
+        print(data)
+        data = await websocket.recv()
+        print(data)
+        await websocket.send(sub)
+
         play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
         trackno += 1
         play(join(SFX_DIR, 'powerup.flac'), until_end=True)
 
-        #Orientation Telescope
+        # Orientation Telescope
         play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
         trackno += 1
         play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
@@ -79,14 +91,21 @@ async def main_procedure():
             data = json.loads(data)
             actionPerformed = read_action_from_leap(data, 'moveLeft')
         await websocket.send(unsub)
-        while await websocket.recv():
-            pass
         play(join(SFX_DIR, 'confirm.flac'), until_end=True)
+
+    async with websockets.connect('ws://127.0.0.1:6437/v6.json') as websocket:
+        sub = json.dumps({'focused': True})
+        unsub = json.dumps({'focused': False})
+        data = await websocket.recv()
+        print(data)
+        data = await websocket.recv()
+        print(data)
+        await websocket.send(sub)
         play(join(SFX_DIR, 'rouages.flac'), until_end=True, left_center_right=RIGHT, back_center_front=BACK)
         play(join(SFX_DIR, 'rouages.flac'), until_end=True, left_center_right=RIGHT, back_center_front=FRONT)
         play(join(SFX_DIR, 'rouages.flac'), until_end=True, left_center_right=LEFT, back_center_front=FRONT)
 
-        #Vent et Ouverture dôme
+        # Vent et Ouverture dôme
         play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
         trackno += 1
         play(join(SFX_DIR, 'wind.flac'), until_end=True, rotate=True)
@@ -99,12 +118,19 @@ async def main_procedure():
             data = json.loads(data)
             actionPerformed = read_action_from_leap(data, 'handDetected')
         await websocket.send(unsub)
-        while await websocket.recv():
-            pass
         play(join(SFX_DIR, 'confirm.flac'), until_end=True)
+
+    async with websockets.connect('ws://127.0.0.1:6437/v6.json') as websocket:
+        sub = json.dumps({'focused': True})
+        unsub = json.dumps({'focused': False})
+        data = await websocket.recv()
+        print(data)
+        data = await websocket.recv()
+        print(data)
+        await websocket.send(sub)
         play(join(SFX_DIR, 'opening.flac'), until_end=True)
 
-        #Explication passage première planète
+        # Explication passage première planète
         play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
         trackno += 1
         await websocket.send(sub)
@@ -114,12 +140,19 @@ async def main_procedure():
             data = json.loads(data)
             actionPerformed = read_action_from_leap(data, 'moveLeft')
         await websocket.send(unsub)
-        while await websocket.recv():
-            pass
+    async with websockets.connect('ws://127.0.0.1:6437/v6.json') as websocket:
+        sub = json.dumps({'focused': True})
+        unsub = json.dumps({'focused': False})
+        data = await websocket.recv()
+        print(data)
+        data = await websocket.recv()
+        print(data)
+        await websocket.send(sub)
+
         play(join(SFX_DIR, 'confirm.flac'), until_end=True)
         ambiant.stop()
 
-        #Planète 1
+        # Planète 1
         ambiant = play(join(MUS_DIR, 'drifting_through_space.flac'), loop=True)
         ambiant.set_gain(0.2)
         play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
@@ -134,12 +167,19 @@ async def main_procedure():
             data = json.loads(data)
             actionPerformed = read_action_from_leap(data, 'moveLeft')
         await websocket.send(unsub)
-        while await websocket.recv():
-            pass
+
+    async with websockets.connect('ws://127.0.0.1:6437/v6.json') as websocket:
+        sub = json.dumps({'focused': True})
+        unsub = json.dumps({'focused': False})
+        data = await websocket.recv()
+        print(data)
+        data = await websocket.recv()
+        print(data)
+        await websocket.send(sub)
         play(join(SFX_DIR, 'confirm.flac'), until_end=True)
         ambiant.stop()
 
-        #Planète 2
+        # Planète 2
         ambiant = play(join(MUS_DIR, 'drift.flac'), loop=True)
         ambiant.set_gain(0.2)
         play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
@@ -154,12 +194,19 @@ async def main_procedure():
             data = json.loads(data)
             actionPerformed = read_action_from_leap(data, 'moveLeft')
         await websocket.send(unsub)
-        while await websocket.recv():
-            pass
+
+    async with websockets.connect('ws://127.0.0.1:6437/v6.json') as websocket:
+        sub = json.dumps({'focused': True})
+        unsub = json.dumps({'focused': False})
+        data = await websocket.recv()
+        print(data)
+        data = await websocket.recv()
+        print(data)
+        await websocket.send(sub)
         play(join(SFX_DIR, 'confirm.flac'), until_end=True)
         ambiant.stop()
 
-        #Planète 3
+        # Planète 3
         ambiant = play(join(MUS_DIR, 'derelict.flac'), loop=True)
         ambiant.set_gain(0.01)
         play(join(BLA_DIR, f'{trackno}.flac'), until_end=True)
@@ -178,23 +225,24 @@ def format_json(data):
 
 
 def read_action_from_leap(data, action_string):
-        if action_string == 'grabStrength':
-            return data['hands'] and (data['hands'][0][action_string] == 1.0)
-        elif action_string == 'handDetected':
-            return data['hands']
-        elif action_string == 'pinchStrength':
-            return data['hands'] and (data['hands'][0]['grabStrength']) < 0.5 and (data['hands'][0][action_string] == 1.0)
-        elif action_string == 'moveRight':
-            return data['hands'] and (data['hands'][0]['palmVelocity'][0] > 150)
-        elif action_string == 'moveLeft':
-            return data['hands'] and (data['hands'][0]['palmVelocity'][0] < -150)
-        elif action_string == 'moveUp':
-            return data['hands'] and (data['hands'][0]['palmVelocity'][1] > 150)
-        elif action_string == 'moveDown':
-            return data['hands'] and (data['hands'][0]['palmVelocity'][1] < -150)
-        elif action_string == 'moveBack':
-            return  data['hands'] and (data['hands'][0]['palmVelocity'][2] > 150)
-        elif action_string == 'moveFront':
-            return data['hands'] and (data['hands'][0]['palmVelocity'][2] < -150)
+    if action_string == 'grabStrength':
+        return data['hands'] and (data['hands'][0][action_string] == 1.0)
+    elif action_string == 'handDetected':
+        return data['hands']
+    elif action_string == 'pinchStrength':
+        return data['hands'] and (data['hands'][0]['grabStrength']) < 0.5 and (data['hands'][0][action_string] == 1.0)
+    elif action_string == 'moveRight':
+        return data['hands'] and (data['hands'][0]['palmVelocity'][0] > 150)
+    elif action_string == 'moveLeft':
+        return data['hands'] and (data['hands'][0]['palmVelocity'][0] < -150)
+    elif action_string == 'moveUp':
+        return data['hands'] and (data['hands'][0]['palmVelocity'][1] > 150)
+    elif action_string == 'moveDown':
+        return data['hands'] and (data['hands'][0]['palmVelocity'][1] < -150)
+    elif action_string == 'moveBack':
+        return data['hands'] and (data['hands'][0]['palmVelocity'][2] > 150)
+    elif action_string == 'moveFront':
+        return data['hands'] and (data['hands'][0]['palmVelocity'][2] < -150)
+
 
 asyncio.get_event_loop().run_until_complete(main_procedure())
